f6b0f8367502b134cfa596833d41c5201d563586
Michael McAtee <jmmcatee@gmail.com>
2014-09-21 23:48:16 -0500

    changing API

--- govsphereutils.go
+++ govsphereutils.go
@@ -6,9 +6,54 @@ import (
 	"github.com/vmware/govmomi/vim25/types"
 )
 
-type SearchFunction func(govmomi.Reference) bool
+type Searcher interface {
+	Compare(*govmomi.Client, govmomi.Reference) bool
+}
 
-func RecursiveSearch(c *govmomi.Client, sf SearchFunction) (govmomi.Reference, error) {
+type nameSearcher struct {
+	Name string
+	Type string
+}
+
+func (v nameSearcher) Compare(c *govmomi.Client, ref govmomi.Reference) bool {
+	// Check for VM Type
+	if ref.Reference().Type != v.Type {
+		return false
+	}
+
+	switch ref.Reference().Type {
+	case "VirtualMachine":
+		// Check for name
+		var vm mo.VirtualMachine
+		c.Properties(ref.Reference(), nil, &vm)
+
+		if vm.Name == v.Name {
+			return true
+		}
+	case "HostSystem":
+		var h mo.HostSystem
+		c.Properties(ref.Reference(), nil, &h)
+		if h.Name == v.Name {
+			return true
+		}
+	}
+
+	return false
+}
+
+func FindVMByName(c *govmomi.Client, name string) (govmomi.Reference, error) {
+	s := nameSearcher{name, "VirtualMachine"}
+
+	return RecursiveSearch(c, s)
+}
+
+func FindHostByName(c *govmomi.Client, name string) (govmomi.Reference, error) {
+	s := nameSearcher{name, "HostSystem"}
+
+	return RecursiveSearch(c, s)
+}
+
+func RecursiveSearch(c *govmomi.Client, s Searcher) (govmomi.Reference, error) {
 	// Take root vSphere folder and loop through each datacenter
 	children, err := c.RootFolder().Children(c)
 	if err != nil {
@@ -17,7 +62,7 @@ func RecursiveSearch(c *govmomi.Client, sf SearchFunction) (govmomi.Reference, e
 
 	// Loop through all datacenters and recursively search their trees
 	for _, v := range children {
-		ref, err := recursion(c, v, sf)
+		ref, err := recursion(c, v, s)
 		if err != nil {
 			return nil, err
 		}
@@ -31,9 +76,9 @@ func RecursiveSearch(c *govmomi.Client, sf SearchFunction) (govmomi.Reference, e
 }
 
 // recursion is meant to take a datacenter and recursively move down the tree
-func recursion(c *govmomi.Client, ref govmomi.Reference, sf SearchFunction) (govmomi.Reference, error) {
+func recursion(c *govmomi.Client, ref govmomi.Reference, s Searcher) (govmomi.Reference, error) {
 	// First thing is to check if we have found our reference
-	if sf(ref) {
+	if s.Compare(c, ref) {
 		return ref, nil
 	}
 
@@ -47,7 +92,7 @@ func recursion(c *govmomi.Client, ref govmomi.Reference, sf SearchFunction) (gov
 		}
 
 		// Walk the VM folder
-		found, err := recursion(c, dcFolders.VmFolder, sf)
+		found, err := recursion(c, dcFolders.VmFolder, s)
 		if err != nil {
 			return nil, err
 		}
@@ -56,7 +101,7 @@ func recursion(c *govmomi.Client, ref govmomi.Reference, sf SearchFunction) (gov
 		}
 
 		// Walk the Host folder
-		found, err = recursion(c, dcFolders.HostFolder, sf)
+		found, err = recursion(c, dcFolders.HostFolder, s)
 		if err != nil {
 			return nil, err
 		}
@@ -65,7 +110,7 @@ func recursion(c *govmomi.Client, ref govmomi.Reference, sf SearchFunction) (gov
 		}
 
 		// Walk the Datastore Folder
-		found, err = recursion(c, dcFolders.DatastoreFolder, sf)
+		found, err = recursion(c, dcFolders.DatastoreFolder, s)
 		if err != nil {
 			return nil, err
 		}
@@ -74,7 +119,7 @@ func recursion(c *govmomi.Client, ref govmomi.Reference, sf SearchFunction) (gov
 		}
 
 		// walk the Network folder
-		found, err = recursion(c, dcFolders.NetworkFolder, sf)
+		found, err = recursion(c, dcFolders.NetworkFolder, s)
 		if err != nil {
 			return nil, err
 		}
@@ -90,7 +135,7 @@ func recursion(c *govmomi.Client, ref govmomi.Reference, sf SearchFunction) (gov
 		}
 
 		for _, v := range children {
-			found, err := recursion(c, v, sf)
+			found, err := recursion(c, v, s)
 			if err != nil {
 				return nil, err
 			}
@@ -104,7 +149,7 @@ func recursion(c *govmomi.Client, ref govmomi.Reference, sf SearchFunction) (gov
 
 		for _, v := range sp.ChildEntity {
 			newRef := newReference(v)
-			found, err := recursion(c, newRef, sf)
+			found, err := recursion(c, newRef, s)
 			if err != nil {
 				return nil, err
 			}
@@ -118,7 +163,7 @@ func recursion(c *govmomi.Client, ref govmomi.Reference, sf SearchFunction) (gov
 
 		for _, v := range ccr.Host {
 			newRef := newReference(v)
-			found, err := recursion(c, newRef, sf)
+			found, err := recursion(c, newRef, s)
 			if err != nil {
 				return nil, err
 			}
@@ -132,7 +177,7 @@ func recursion(c *govmomi.Client, ref govmomi.Reference, sf SearchFunction) (gov
 
 		for _, v := range vDVS.Portgroup {
 			newRef := newReference(v)
-			found, err := recursion(c, newRef, sf)
+			found, err := recursion(c, newRef, s)
 			if err != nil {
 				return nil, err
 			}
